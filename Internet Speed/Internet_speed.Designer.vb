1011b4ab2330324ac45449905a83221f2c5ef105
K-c00per <katelyn.cooper@tntns.com>
2023-03-14 11:35:17 -0400

    Add project files.

---
+++ Internet Speed/Internet_speed.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Internet_speed
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Internet_speed))
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.btnSpeed = New System.Windows.Forms.Button()
+        Me.lstInternetSpeed = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(290, 53)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(330, 29)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Internet Speed Test Survey"
+        '
+        'btnSpeed
+        '
+        Me.btnSpeed.BackColor = System.Drawing.Color.DarkKhaki
+        Me.btnSpeed.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpeed.ForeColor = System.Drawing.Color.Olive
+        Me.btnSpeed.Location = New System.Drawing.Point(335, 111)
+        Me.btnSpeed.Name = "btnSpeed"
+        Me.btnSpeed.Size = New System.Drawing.Size(209, 47)
+        Me.btnSpeed.TabIndex = 1
+        Me.btnSpeed.Text = "Enter Internet Speed"
+        Me.btnSpeed.UseVisualStyleBackColor = False
+        '
+        'lstInternetSpeed
+        '
+        Me.lstInternetSpeed.FormattingEnabled = True
+        Me.lstInternetSpeed.Location = New System.Drawing.Point(640, 85)
+        Me.lstInternetSpeed.Name = "lstInternetSpeed"
+        Me.lstInternetSpeed.Size = New System.Drawing.Size(98, 199)
+        Me.lstInternetSpeed.TabIndex = 2
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(563, 409)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(86, 27)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverage.Location = New System.Drawing.Point(425, 458)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(243, 24)
+        Me.lblAverage.TabIndex = 4
+        Me.lblAverage.Text = "Average Internet Speed: "
+        Me.lblAverage.Visible = False
+        '
+        'Internet_speed
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(786, 533)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstInternetSpeed)
+        Me.Controls.Add(Me.btnSpeed)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "Internet_speed"
+        Me.Text = "Internet Speed Test Surevy for Home Users"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents btnSpeed As Button
+    Friend WithEvents lstInternetSpeed As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblAverage As Label
+End Class
---
+++ Internet Speed/Internet_speed.vb
@@ -0,0 +1,64 @@
+﻿Public Class Internet_speed
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblAverage.Text = ""
+        lstInternetSpeed.Items.Clear()
+        btnSpeed.Enabled = True
+    End Sub
+
+    Private Sub btnSpeed_Click(sender As Object, e As EventArgs) Handles btnSpeed.Click
+        'Declare and initialize varaibles
+        Dim strSpeed As String
+        Dim decSpeed As Decimal
+        Dim decSumOfSpeeds As Decimal
+        Dim decAverage As Decimal = 0D
+
+        Dim strIBoxMsg As String = "Enter the number of Mbps of your home Internet speed user #"
+        Dim strIBoxTitle As String = "Internet Speed"
+        Dim strNotNumericErrMsg As String = "Error - Enter the speed of your home Internet connection"
+        Dim strNegErrMsg As String = "Error - Enter a valid speed"
+
+        'Declare and initialize loop varaibles
+        Dim intMaxEntries As Integer = 10
+        Dim intEntries As Integer = 1
+
+        strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+
+        'Do until intNumberOfEntries > intMaxNumberOfEntries
+        'or until strSpeed is empty (use Cancel Button property)
+        Do Until intEntries > intMaxEntries Or strSpeed = ""
+            'validates that the speed is numeric
+            If IsNumeric(strSpeed) Then
+                decSpeed = Convert.ToDecimal(strSpeed)
+                'validates that decSpeed is positive
+                If decSpeed > 0 Then
+                    lstInternetSpeed.Items.Add(decSpeed)
+                    decSumOfSpeeds += decSpeed
+                    intEntries += 1
+                    strIBoxMsg = strIBoxMsg
+                Else
+                    'if the speed is not positive, gives error message 
+                    strIBoxMsg = strNegErrMsg
+                End If
+            Else
+                'if not a number, give error message
+                strIBoxMsg = strNotNumericErrMsg
+            End If
+
+            If intEntries <= intMaxEntries Then
+                strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+            End If
+        Loop
+
+        'makes label visable
+        lblAverage.Visible = True
+        'calculates and displays average speed
+        If intEntries > 1 Then
+            decAverage = decSumOfSpeeds / (intEntries - 1)
+            lblAverage.Text = "Average Internet Speed: " & decAverage.ToString("F2") & " Mbps"
+        Else
+            lblAverage.Text = "No Speed Values Entered"
+        End If
+
+        btnSpeed.Enabled = False
+    End Sub
+End Class
